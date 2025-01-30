d14fceb156511ba89e9b360d378fd2916bf928c9
Nennae <sezenerdal@outlook.com>
2025-01-30 22:20:26 +0100

    module added + separate script folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,7 @@
-import { fetchBooks, fetchSearchBooks } from "./data.js";
+import {fetchSearchBooks } from "./data.js";
+import { checkBooks } from "./localStorage.js";
 
-async function checkBooks() {
-  // check if there are books in localStorage
-  let selection_books = JSON.parse(localStorage.getItem("selection_books"));
-  if (selection_books) {
-    // if there are, display them
-    displayBooks(selection_books);
-  } else {
-    // else, fetch books and display them
-    try {
-      const fetchedBooks = await fetchBooks();
-      localStorage.setItem("selection_books", JSON.stringify(fetchedBooks));
-      selection_books = fetchedBooks;
-      displayBooks(selection_books);
-    } catch (error) {
-      console.error("Failed to fetch and display books:", error);
-    }
-  }
-}
-
-function displayBooks(books) {
+export function displayBooks(books) {
       console.log("Displaying books:", books); 
 
       if (!Array.isArray(books)) {
@@ -44,7 +26,9 @@ function displayBooks(books) {
       });
      
 }
-checkBooks();
+
+// Call checkBooks to fetch and display books
+checkBooks(displayBooks);
 
 function displaySearchedBooks() {
       fetchSearchBooks("Erin Morgenstern");
---
+++ scripts/localStorage.js
@@ -0,0 +1,21 @@
+import { fetchBooks } from "./data.js";
+import { displayBooks } from "./app.js";
+
+export async function checkBooks() {
+  // check if there are books in localStorage
+  let selection_books = JSON.parse(localStorage.getItem("selection_books"));
+  if (selection_books) {
+    // if there are, display them
+    displayBooks(selection_books);
+  } else {
+    // else, fetch books and display them
+    try {
+      const fetchedBooks = await fetchBooks();
+      localStorage.setItem("selection_books", JSON.stringify(fetchedBooks));
+      selection_books = fetchedBooks;
+      displayBooks(selection_books);
+    } catch (error) {
+      console.error("Failed to fetch and display books:", error);
+    }
+  }
+}
